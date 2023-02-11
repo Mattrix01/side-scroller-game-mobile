c90e98e978a580d63829683012ea84f5e852ae32
Mattrix01 <emjay.knight@gmail.com>
2023-02-11 11:35:15 +0000

    player jumping and falling

--- script.js
+++ script.js
@@ -55,6 +55,9 @@ window.addEventListener("load", function () {
       this.frameY = 0;
       // when speed is posotive player will move right, when negative move to left on horizonal x.
       this.speed = 0;
+      // for vertical movement on up arrow
+      this.vy = 0;
+      this.weight = 1;
     }
     draw(context) {
       context.fillStyle = "white";
@@ -73,20 +76,41 @@ window.addEventListener("load", function () {
     }
     update(input) {
       // every call of update method increment x coordinates by speed of posotive go right negative number go left.
-      // horiztonal movment
-      this.x += this.speed;
+      // controls
       if (input.keys.indexOf("ArrowRight") > -1) {
         // if index of the keys input is arrow up, run this code.
         this.speed = 5;
       } else if (input.keys.indexOf("ArrowLeft") > -1) {
         this.speed = -5;
+        // && so player can only jump when on solid ground
+      } else if (input.keys.indexOf("ArrowUp") > -1 && this.onGround()) {
+        // size of jump
+        this.vy -= 30;
       } else {
         this.speed = 0;
       }
       // to stop player going off screen
+      // horizontal movment
+      this.x += this.speed;
       if (this.x < 0) this.x = 0;
       else if (this.x > this.gameWidth - this.width)
         this.x = this.gameWidth - this.width;
+      // vertical movement
+      this.y += this.vy;
+      // checking player jump state, if player is not on the ground in the air, take this.vy and increase by this.weight
+      if (!this.onGround()) {
+        this.vy += this.weight;
+      } else {
+        // reswet velocity when player on round to stop vertical movement.
+        this.vy = 0;
+      }
+      // vertical boundry for jump so doesn't go through floor
+      if (this.y > this.gameHeight - this.height)
+        this.y = this.gameHeight - this.height;
+    }
+    // utility method, checking player jump state
+    onGround() {
+      return this.y >= this.gameHeight - this.height;
     }
   }
 

1de6318155b4bdc8e4733710664c82108e781813
Mattrix01 <emjay.knight@gmail.com>
2023-02-17 15:24:14 +0000

    fixed frames on sprites, before jump fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", function () {
   const canvas = this.document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
-  canvas.width = 800;
+  canvas.width = 1000;
   canvas.height = 720;
   let enemies = [];
   let score = 0;
@@ -48,21 +48,22 @@ window.addEventListener("load", function () {
     constructor(gameWidth, gameHeight) {
       this.gameWidth = gameWidth;
       this.gameHeight = gameHeight;
-      this.width = 200;
+      // 200 7023/50 = 140.46
+      this.width = 140.46;
       this.height = 200;
       this.x = 0;
       this.y = this.gameHeight - this.height;
       // bringing in sprite image
       this.image = document.getElementById("playerImage");
       this.frameX = 0;
-      this.maxFrame = 8;
+      this.maxFrame = 49;
       this.frameY = 0;
       // will affect how fast we swap between animation frames.
-      this.fps = 20;
+      this.fps = 9000;
       this.frameTimer = 0;
       // how long each frame lasts
       this.frameInterval = 1000 / this.fps;
-      // when speed is posotive player will move right, when negative move to left on horizonal x.
+      // when speed is positive player will move right, when negative move to left on horizonal x.
       this.speed = 0;
       // for vertical movement on up arrow
       this.vy = 0;
@@ -149,12 +150,12 @@ window.addEventListener("load", function () {
       if (!this.onGround()) {
         this.vy += this.weight;
         // dealing with sprite sheet jump animation less frames
-        this.maxFrame = 5;
+        this.maxFrame = 15;
         this.frameY = 1;
       } else {
         // reset velocity when player on round to stop vertical movement.
         this.vy = 0;
-        this.maxFrame = 8;
+        this.maxFrame = 49;
         this.frameY = 0;
       }
       // vertical boundry for jump so doesn't go through floor
@@ -176,7 +177,7 @@ window.addEventListener("load", function () {
       this.x = 0;
       this.y = 0;
       // dimensions of background image
-      this.width = 2400;
+      this.width = 2443;
       this.height = 720;
       this.speed = 7;
     }
@@ -206,14 +207,15 @@ window.addEventListener("load", function () {
     constructor(gameWidth, gameHeight) {
       this.gameWidth = gameWidth;
       this.gameHeight = gameHeight;
-      this.width = 160;
-      this.height = 119;
+      // 140 1755/16 = 109.6
+      this.width = 109.6;
+      this.height = 140;
       this.image = document.getElementById("enemyImage");
       this.x = this.gameWidth;
       this.y = this.gameHeight - this.height;
       // for horizontal navigation in sprite sheet
       this.frameX = 0;
-      this.maxFrame = 5;
+      this.maxFrame = 15;
       // will affect how fast we swap between animation frames.
       this.fps = 20;
       this.frameTimer = 0;
